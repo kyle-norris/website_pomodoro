7e25fe7dc78d3b40921e22dc722e90acf3a66147
kyle-norris <kyle.coding@outlook.com>
2021-01-23 11:32:09 -0500

    Created logic for countdown timer
    Used useEffect() and setInterval() to create a repeating countdown.

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,12 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  const [duration, setDuration] = useState(1500);
+  const [seconds, setSeconds] = useState(duration);
+  const [running, setRunning] = useState(true);
+
+  function toggleRunning() {
+    setRunning(!running);
+  }
+
+  function reset() {
+    setSeconds(duration);
+    setRunning(false);
+  }
+
+  // Update the number of seconds left:
+  useEffect(() => {
+    let interval = null;
+    if (running) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval); // This is run when it is time to clean up
+  }, [running, seconds]);
+
+  function getMinutes() {
+    let m = 0;
+    m = Math.trunc(seconds / 60);
+    m = ("00" + m).slice(-2);
+
+    return m;
+  }
+
+  function getSeconds() {
+    let s = 0;
+    s = Math.trunc(seconds % 60);
+    s = ("00" + s).slice(-2);
+
+    return s;
+  }
 
   return (
-    <div className="app">
-      <h1>Pomodooro App</h1>
+    <>
+    <div className="time">
+      {getMinutes()}:{getSeconds()}
     </div>
+    <button onClick={toggleRunning}>
+      toggle
+    </button>
+    <button onClick={reset}>
+      reset
+    </button>
+      {/* logo */}
+      {/* Category Selection (pomodoro, short break, long break) */}
+      {/* timer */}
+      {/* settings button */}
+    </>
   );
 };
 
---
+++ pomodoro/src/Components/CategoryNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryNav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryNav
